6252e5ee2a8d75374f289bc1efa6677d525adf09
Amir Hossain <amir508_du@yahoo.com>
2022-05-24 05:22:13 +0600

    add new order api created

--- index.js
+++ index.js
@@ -30,25 +30,64 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1
 });
 
+//Verify JWT
+// const verifyJWT = (req, res, next) => {
+//     //check authorization
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({
+//             message: 'Unauthorized access'
+//         })
+//     }
+//     //verify authorization
+//     const token = authHeader.split(' ')[1]; 
+//     jwt.verify(token, process.env.SECRET_KEY_TOKEN, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({
+//                 message: 'Forbidden access'
+//             })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     });
+// }
+
 async function run() {
     try{
         await client.connect();
         // console.log('db connected')
         const mirrorCollection = client.db('Raiyan_Auto_Mirror').collection('mirrors');
+        const orderCollection = client.db('Raiyan_Auto_Mirror').collection('orders')
 
-        //get all mirrors API
+        // //AUTH
+        // app.post('/login', async (req, res) => {
+        //     const user = req.body;
+        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '1d'
+        //     });
+        //     res.send({accessToken});
+        // });
+
+        //get all items API
         app.get('/item', async (req, res) => {
             const result = await mirrorCollection.find().toArray();
             res.send(result);
         });
  
-        //get mirror by user Id API
+        //get item by user Id API
         app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await mirrorCollection.findOne(query);
             res.send(result);
-        })
+        });
+
+        //add new order API
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
     }
     finally{
 
